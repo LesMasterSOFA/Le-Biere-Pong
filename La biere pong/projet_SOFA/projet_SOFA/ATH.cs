69ca215fe12b8fe4481c7dcc9daf080ded62787e
olivierlauzon85 <olivierlauzon85@gmail.com>
2016-02-01 10:38:21 -0500

    début ath

--- La biere pong/projet_SOFA/projet_SOFA/ATH.cs
+++ La biere pong/projet_SOFA/projet_SOFA/ATH.cs
@@ -12,35 +12,21 @@ using Microsoft.Xna.Framework.Media;
 
 namespace AtelierXNA
 {
-    /// <summary>
-    /// This is a game component that implements IUpdateable.
-    /// </summary>
     public class ATH : Microsoft.Xna.Framework.DrawableGameComponent
     {
+        BoutonDeCommande BoutonPause { get; set; }
+        BoutonDeCommande BoutonLancer { get; set; }
+        string NombreDePoints { get; set; }
         public ATH(Game game)
-            : base(game)
-        {
-            // TODO: Construct any child components here
-        }
-
-        /// <summary>
-        /// Allows the game component to perform any initialization it needs to before starting
-        /// to run.  This is where it can query for any required services and load content.
-        /// </summary>
+            : base(game){}
         public override void Initialize()
         {
-            // TODO: Add your initialization code here
-
+            BoutonLancer = new BoutonDeCommande(Game,"Lancer","Arial20","BoutonBleu","BoutonBleuPale",)
             base.Initialize();
         }
 
-        /// <summary>
-        /// Allows the game component to update itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         public override void Update(GameTime gameTime)
         {
-            // TODO: Add your update code here
 
             base.Update(gameTime);
         }
