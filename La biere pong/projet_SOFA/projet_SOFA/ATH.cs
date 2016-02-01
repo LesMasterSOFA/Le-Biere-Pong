393815d5e404ccdcfbcc384519d87e68c2debe97
olivierlauzon85 <olivierlauzon85@gmail.com>
2016-02-01 10:48:19 -0500

    ATH v2
    ajout de boutons lancer et pause

--- La biere pong/projet_SOFA/projet_SOFA/ATH.cs
+++ La biere pong/projet_SOFA/projet_SOFA/ATH.cs
@@ -21,7 +21,12 @@ namespace AtelierXNA
             : base(game){}
         public override void Initialize()
         {
-            BoutonLancer = new BoutonDeCommande(Game,"Lancer","Arial20","BoutonBleu","BoutonBleuPale",)
+            Vector2 positionBoutonLancer = new Vector2(Game.Window.ClientBounds.Width - 60, Game.Window.ClientBounds.Height - 40);
+            Vector2 positionBoutonPause = new Vector2(Game.Window.ClientBounds.Width - 60, 40);
+            BoutonLancer = new BoutonDeCommande(Game, "Lancer", "Arial20", "BoutonBleu", "BoutonBleuPale", positionBoutonLancer, false, null);
+            BoutonPause = new BoutonDeCommande(Game, "Pause", "Arial20", "BoutonBleu", "BoutonBleuPale", positionBoutonPause, true, null);
+            Game.Components.Add(BoutonLancer);
+            Game.Components.Add(BoutonPause);
             base.Initialize();
         }
 
