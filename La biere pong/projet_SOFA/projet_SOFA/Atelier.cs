766d1c0b50ec70d7bc4aaaf17dea83519053fd15
olivierlauzon85 <olivierlauzon85@gmail.com>
2016-02-09 22:20:51 -0500

    Nouvelles images + Caméra
    Environnement temporaire + caméra désormais dans environnement et non
    dans Atelier (Pas utile dans les menus, etc.)

--- La biere pong/projet_SOFA/projet_SOFA/Atelier.cs
+++ La biere pong/projet_SOFA/projet_SOFA/Atelier.cs
@@ -22,7 +22,6 @@ namespace AtelierXNA
       RessourcesManager<Texture2D> GestionnaireDeTextures { get; set; }
       RessourcesManager<Model> GestionnaireDeModèles { get; set; }
       RessourcesManager<Effect> GestionnaireDeShaders { get; set; }
-      Caméra CaméraJeu { get; set; }
 
       public InputManager GestionInput { get; private set; }
 
@@ -38,24 +37,17 @@ namespace AtelierXNA
       protected override void Initialize()
       {
          
-         Vector3 positionCaméra = new Vector3(0, 90, 65);
-         Vector3 cibleCaméra = new Vector3(0, 60, 0);
 
          GestionnaireDeFonts = new RessourcesManager<SpriteFont>(this, "Fonts");
          GestionnaireDeTextures = new RessourcesManager<Texture2D>(this, "Textures");
          GestionnaireDeModèles = new RessourcesManager<Model>(this, "Models");
          GestionnaireDeShaders = new RessourcesManager<Effect>(this, "Effects"); 
          GestionInput = new InputManager(this);
-         CaméraJeu = new CaméraSubjective(this, positionCaméra, cibleCaméra, Vector3.Up, INTERVALLE_MAJ_STANDARD);
 
-         //Components.Add(new Menu(this));
-         //Components.Add(new ATH(this));
-        
+
          Components.Add(GestionInput);
-         Components.Add(CaméraJeu);
          Components.Add(new Afficheur3D(this));
          Components.Add(new GestionEnvironnement(this));
-        
          Components.Add(new AfficheurFPS(this, "Arial20", Color.Gold, INTERVALLE_CALCUL_FPS));
 
          Services.AddService(typeof(Random), new Random());
@@ -64,7 +56,6 @@ namespace AtelierXNA
          Services.AddService(typeof(RessourcesManager<Model>), GestionnaireDeModèles);
          Services.AddService(typeof(RessourcesManager<Effect>), GestionnaireDeShaders);
          Services.AddService(typeof(InputManager), GestionInput);
-         Services.AddService(typeof(Caméra), CaméraJeu);
          GestionSprites = new SpriteBatch(GraphicsDevice);
          Services.AddService(typeof(SpriteBatch), GestionSprites);
          base.Initialize();
