266750d027a1610ce875d39e375e45c972f31e65
Simon Legault <simon_legault@hotmail.ca>
2016-02-15 14:27:30 -0500

    Ajout librairie lidgren dans solution

---
+++ La biere pong/projet_SOFA/projet_SOFA/NetworkManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+
+
+namespace AtelierXNA
+{
+    public class NetworkManager : Microsoft.Xna.Framework.GameComponent
+    {
+        public NetworkManager(Game game)
+            : base(game)
+        {
+        }
+
+        public override void Initialize()
+        {
+
+            base.Initialize();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+            base.Update(gameTime);
+        }
+    }
+}
