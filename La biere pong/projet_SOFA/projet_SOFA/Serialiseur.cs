afa7534c4532917e479a4fc7744915972075c75f
BobTheHornyProgrammer <simon_legault@hotmail.ca>
2016-03-07 10:50:16 -0500

    Création classe désérialiseur

---
+++ La biere pong/projet_SOFA/projet_SOFA/Serialiseur.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AtelierXNA
+{
+    class Serialiseur
+    {
+        static void Sérialiser()
+        {
+
+        }
+
+        static void Désérialiser()
+        {
+
+        }
+    }
+}
