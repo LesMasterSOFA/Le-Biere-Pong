6afa4111f9c92f67a96a03364d42ee8805da47d4
BobTheHornyProgrammer <simon_legault@hotmail.ca>
2016-03-09 09:59:20 -0500

    Début séréaliseur

--- La biere pong/projet_SOFA/projet_SOFA/Serialiseur.cs
+++ La biere pong/projet_SOFA/projet_SOFA/Serialiseur.cs
@@ -8,16 +8,61 @@ using System.Runtime.Serialization.Formatters.Binary;
 
 namespace AtelierXNA
 {
-    class Serialiseur
+    static class Serialiseur
     {
-        static void Sérialiser()
+        static MemoryStream Sérialiser(object ObjToSerialize)
         {
+            MemoryStream streamMemoire = new MemoryStream();
+            BinaryFormatter formatter = new BinaryFormatter();
 
+            try
+            {
+                formatter.Serialize(streamMemoire, ObjToSerialize);
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Erreur dans la sérialisation de: " + e.ToString());
+            }
+
+            finally
+            {
+                if (streamMemoire != null)
+                    streamMemoire.Close();
+            }
+            return streamMemoire;
         }
 
-        static void Désérialiser()
+        static T Désérialiser<T>(MemoryStream streamMemoire)
         {
+            BinaryFormatter formatter = new BinaryFormatter();
 
+            try
+            {
+                return (T)formatter.Deserialize(streamMemoire);
+            }
+
+            catch
+            {
+                return default(T);
+            }
+
+            finally
+            {
+                if (streamMemoire != null)
+                    streamMemoire.Close();
+            }
+        }
+
+        static public byte[] ObjToByteArray(object objToTransform)
+        {
+            return Sérialiser(objToTransform).GetBuffer();
+        }
+
+        static public T ByteArrayToObj<T>(byte[] byteArray)
+        {
+            MemoryStream streamMemoire = new MemoryStream(byteArray);
+            return Désérialiser<T>(streamMemoire); 
         }
     }
 }
