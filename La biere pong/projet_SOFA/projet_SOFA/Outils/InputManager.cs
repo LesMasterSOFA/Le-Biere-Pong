2214730ce589b7bbd569e51879a73fdb7fe07d16
BobTheHornyProgrammer <simon_legault@hotmail.ca>
2016-03-30 11:49:14 -0400

    Ajout outils dans dossier Outils

---
+++ La biere pong/projet_SOFA/projet_SOFA/Outils/InputManager.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace AtelierXNA
+{
+   public class InputManager : Microsoft.Xna.Framework.GameComponent
+   {
+      Keys[] AnciennesTouches { get; set; }
+      Keys[] NouvellesTouches { get; set; }
+      MouseState ÉtatSouris { get; set; }
+      MouseState AncienneÉtatSouris { get; set; }
+      MouseState NouvelleÉtatSouris { get; set; }
+      KeyboardState ÉtatClavier { get; set; }
+
+      public InputManager(Game game)
+         : base(game)
+      { }
+
+      public override void Initialize()
+      {
+         AnciennesTouches = new Keys[0];
+         NouvellesTouches = new Keys[0];
+         ÉtatSouris = Mouse.GetState();
+         base.Initialize();
+      }
+
+      public override void Update(GameTime gameTime)
+      {
+         AnciennesTouches = NouvellesTouches;
+         ÉtatClavier = Keyboard.GetState();
+         NouvellesTouches = ÉtatClavier.GetPressedKeys();
+         AncienneÉtatSouris = NouvelleÉtatSouris;
+         ÉtatSouris = Mouse.GetState();
+         NouvelleÉtatSouris = ÉtatSouris;
+
+         base.Update(gameTime);
+      }
+
+      public bool EstClavierActivé
+      {
+         get { return NouvellesTouches.Length > 0; }
+      }
+
+      public bool EstEnfoncée(Keys touche)
+      {
+         return ÉtatClavier.IsKeyDown(touche);
+      }
+
+      public bool EstNouvelleTouche(Keys touche)
+      {
+         int NbTouches = AnciennesTouches.Length;
+         bool EstNouvelleTouche = ÉtatClavier.IsKeyDown(touche);
+         int i = 0;
+         while (i < NbTouches && EstNouvelleTouche)
+         {
+            EstNouvelleTouche = AnciennesTouches[i] != touche;
+            ++i;
+         }
+         return EstNouvelleTouche;
+      }
+
+      public bool EstSourisActive
+      {
+         get { return NouvelleÉtatSouris != AncienneÉtatSouris; }
+      }
+      public bool EstAncienClicDroit()
+      {
+         return (NouvelleÉtatSouris.RightButton == ButtonState.Pressed && AncienneÉtatSouris.RightButton == ButtonState.Pressed);
+      }
+      public bool  EstAncienClicGauche()
+      {
+         return (AncienneÉtatSouris.LeftButton == ButtonState.Pressed && NouvelleÉtatSouris.LeftButton == ButtonState.Pressed);
+      }
+      public bool EstNouveauClicDroit()
+      {
+         return (AncienneÉtatSouris.RightButton == ButtonState.Released && NouvelleÉtatSouris.RightButton == ButtonState.Pressed);
+      }
+      public bool EstNouveauClicGauche()
+      {
+         return (AncienneÉtatSouris.LeftButton == ButtonState.Released && NouvelleÉtatSouris.LeftButton == ButtonState.Pressed);
+      }
+      public Point GetPositionSouris()
+      {
+         return new Point(ÉtatSouris.X, ÉtatSouris.Y);
+      }
+   }
+}
---
+++ La biere pong/projet_SOFA/projet_SOFA/Outils/RessourceDeBase.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace AtelierXNA
+{
+   class RessourceDeBase<T> : IEquatable<RessourceDeBase<T>>
+   {
+      public string Nom { get; private set; }
+      public T Ressource { get; private set; }
+      ContentManager Content { get; set; }
+      string Répertoire { get; set; }
+
+      // Ce constructeur est appelé lorsque l'on construit un objet TextureDeBase
+      // à partir d'une image déjà présente en mémoire.
+      public RessourceDeBase(string nom, T ressource)
+      {
+         Nom = nom;
+         Content = null;
+         Répertoire = "";
+         Ressource = ressource;
+      }
+
+      // Ce constructeur est appelé lorsque l'on construit un objet TextureDeBase
+      // à partir du nom d'une image qui sera éventuellement chargé en mémoire.
+      public RessourceDeBase(ContentManager content, string répertoire, string nom)
+      {
+         Nom = nom;
+         Content = content;
+         Répertoire = répertoire;
+         Ressource = default(T);
+      }
+
+      public void Load()
+      {
+         if (Ressource == null)
+         {
+            string NomComplet = Répertoire + "/" + Nom;
+            Ressource = Content.Load<T>(NomComplet);
+         }
+      }
+
+      #region IEquatable<RessourceDeBase<T>> Membres
+
+      public bool Equals(RessourceDeBase<T> other)
+      {
+         return Nom == other.Nom;
+      }
+
+      #endregion
+   }
+}
