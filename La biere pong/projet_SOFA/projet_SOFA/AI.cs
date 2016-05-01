b8d52a1b6e21420b57f28cb1bd7f45a16aea9d83
olivierlauzon85 <olivierlauzon85@gmail.com>
2016-05-01 14:38:16 -0400

    AI qui pogne toutes les verres
    pas tant bon mais mieux que rien

--- La biere pong/projet_SOFA/projet_SOFA/AI.cs
+++ La biere pong/projet_SOFA/projet_SOFA/AI.cs
@@ -20,19 +20,20 @@ namespace AtelierXNA
     {
         const float VITESSE_FACILE = 280f;
         const float VITESSE_MOYEN = 250f;
-        const float VITESSE_DIFFICILE = 210;
+        const float VITESSE_DIFFICILE = 370;
 
         const float ANGLE_HORIZONTAL_FACILE = 10;
         const float ANGLE_HORIZONTAL_MOYEN = 7;
-        const float ANGLE_HORIZONTAL_DIFFICILE = 3;
+        const float ANGLE_HORIZONTAL_DIFFICILE = 300;
 
         const float ANGLE_VERTICAL_FACILE = 50;
         const float ANGLE_VERTICAL_MOYEN = 40;
-        const float ANGLE_VERTICAL_DIFFICILE = 0;
+        const float ANGLE_VERTICAL_DIFFICILE = 30;
 
         public float Vitesse { get; set; }
         public float AngleHorizontal { get; set; }
         public float AngleVertical { get; set; }
+        public int Cpt { get; set; }
 
         ModeDifficulté Difficulté { get; set; }
 
@@ -71,13 +72,18 @@ namespace AtelierXNA
             float[] tableau = new float[3];
 
             bool estPositif = false;
-            Random randPositif = new Random();
-            int prob = randPositif.Next(100);
-            if (prob > 50)
-                estPositif = true;
+            //Random randPositif = new Random();
+            //int prob = randPositif.Next(100);
+            //if (prob > 50)
+            //    estPositif = true;
+            if (Cpt % 2 == 0)
+            {
+               estPositif = true;
+            }
+            Cpt++;
 
             Random vitesseRand = new Random();
-            tableau[0] = vitesseRand.Next(190, (int)Vitesse + 1);
+            tableau[0] = vitesseRand.Next(330, (int)Vitesse + 1);
             tableau[0] = tableau[0] / 100;
              
 
@@ -85,11 +91,15 @@ namespace AtelierXNA
             tableau[1] = angleHorizontalRand.Next(0, (int)AngleHorizontal + 1);
             if (!estPositif)
                 tableau[1] = -tableau[1];
+            tableau[1] = tableau[1] / 100;
+            if (Cpt % 3 == 0)
+               tableau[1] = 0;
+
 
             Random angleVerticalRand = new Random();
-            tableau[2] = angleVerticalRand.Next(0, (int)AngleVertical + 1);
-            if (!estPositif)
-                tableau[2] = -tableau[2];
+            tableau[2] = angleVerticalRand.Next(30, (int)AngleVertical + 1);
+            //if (!estPositif)
+            //    tableau[2] = -tableau[2];
             return tableau;
         }
     }
