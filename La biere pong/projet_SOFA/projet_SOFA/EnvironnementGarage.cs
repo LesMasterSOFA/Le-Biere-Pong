afd03052d79f2aa55a8caace59a19ab6f05c0d0f
Simon Legault <simon_legault@hotmail.ca>
2016-05-03 15:52:40 -0400

    Cleanup using + architecture enviro revue

--- La biere pong/projet_SOFA/projet_SOFA/EnvironnementGarage.cs
+++ La biere pong/projet_SOFA/projet_SOFA/EnvironnementGarage.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
+﻿using Microsoft.Xna.Framework;
 
 namespace AtelierXNA
 {
@@ -18,27 +9,30 @@ namespace AtelierXNA
       ObjetDeBase Urinoir { get; set; }
       ObjetDeBase Table { get; set; }
 
-      public EnvironnementGarage(Game game, GestionEnvironnement gestionEnv, string nomGauche, string nomDroite, string nomPlafond, string nomPlancher,
-                                string nomAvant, string nomArrière, string personnageJoueurPrincipalModel, string personnageJoueurPrincipalTexture,
-                                string personnageJoueurSecondaireModel, string personnageJoueurSecondaireTexture, Vector3 dimensionTable, float distanceVerre, TypePartie typeDePartie)
-         : base(game, gestionEnv, nomGauche, nomDroite, nomPlafond, nomPlancher, nomAvant, nomArrière, personnageJoueurPrincipalModel, personnageJoueurPrincipalTexture,
-                personnageJoueurSecondaireModel, personnageJoueurSecondaireTexture, dimensionTable, distanceVerre, typeDePartie)
+      public EnvironnementGarage(Game game, GestionEnvironnement gestionEnv, string personnageJoueurPrincipalModel, string personnageJoueurPrincipalTexture,
+                                string personnageJoueurSecondaireModel, string personnageJoueurSecondaireTexture, TypePartie typeDePartie)
+         : base(game, gestionEnv, personnageJoueurPrincipalModel, personnageJoueurPrincipalTexture,
+                personnageJoueurSecondaireModel, personnageJoueurSecondaireTexture, typeDePartie)
       { }
 
       public override void Initialize()
       {
          InitialiserModèles();
+         InitialiserMurs(new string[6] {"GaucheGarage","DroiteGarage","PlafondGarage","PlancherGarage","AvantGarage","ArriereGarage"});
+         DimensionTable = new Vector3(0.76f, 0.74f, 1.8f);
+         DistanceVerre = 0.8f;
          base.Initialize();
       }
 
-      void InitialiserModèles()
+      protected override void InitialiserModèles()
       {
-         Table = new ObjetDeBase(Game, "table_plastique", "table_plastique", "Shader", 1, new Vector3(0, 0, 0), new Vector3(0, 0, 0));
-         Etabli = new ObjetDeBase(Game, "etabli", "etabli", "Shader", 1, new Vector3(0, MathHelper.Pi, 0), new Vector3(2.75f, -0.35f, 2.5f));
-         Urinoir = new ObjetDeBase(Game, "urinoir", "urinoir", "Shader", 1, new Vector3(0, MathHelper.PiOver2, -MathHelper.PiOver2), new Vector3(-3.5f, 0.5f, 0));
-         Game.Components.Add(Table);
-         Game.Components.Add(Etabli);
-         Game.Components.Add(Urinoir);
+          Table = new ObjetDeBase(Game, "table_plastique", "table_plastique", "Shader", 1, new Vector3(0, 0, 0), new Vector3(0, 0, 0));
+          Etabli = new ObjetDeBase(Game, "etabli", "etabli", "Shader", 1, new Vector3(0, MathHelper.Pi, 0), new Vector3(2.75f, -0.35f, 2.5f));
+          Urinoir = new ObjetDeBase(Game, "urinoir", "urinoir", "Shader", 1, new Vector3(0, MathHelper.PiOver2, -MathHelper.PiOver2), new Vector3(-3.5f, 0.5f, 0));
+          Game.Components.Add(Table);
+          Game.Components.Add(Etabli);
+          Game.Components.Add(Urinoir);
       }
+
    }
 }
--- La biere pong/projet_SOFA/projet_SOFA/Serialiseur.cs
+++ La biere pong/projet_SOFA/projet_SOFA/Serialiseur.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.IO;
-using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 
 namespace AtelierXNA
